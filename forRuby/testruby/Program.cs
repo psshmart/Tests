d44ede37bb70fc024f1fbed960972fa6f7ac262d
psshmart <65891171+psshmart@users.noreply.github.com>
2020-05-25 11:08:24 +0300

    Add files via upload

---
+++ forRuby/testruby/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace testruby
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Random element = new Random();
+            int elements_int = element.Next(1, 9);
+
+            Random linesnumber = new Random();
+
+            int lines_number_int = linesnumber.Next(4, 10);
+
+
+          
+
+            Row row = new Row(elements_int, lines_number_int);
+
+            
+
+
+
+        }
+
+        
+    }
+}
---
+++ forRuby/testruby/Row.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace testruby
+{
+    public class Row
+    {
+        /// <summary>
+        /// Лист элементов, поступающий в метод NewString
+        /// </summary>
+        public List<int> numbers = new List<int>();
+
+        /// <summary>
+        /// Лист, в который закладывается получающаяся последовательность
+        /// </summary>
+        public List<string> lines = new List<string>();
+
+        /// <summary>
+        /// Счетчик 
+        /// </summary>
+        public int i;
+
+
+        /// <summary>
+        /// Создание последовательности
+        /// </summary>
+        /// <param name="element">первый элемент</param>
+        /// <param name="linesnumber">кол-во строчек</param>
+        public Row(int element, int linesnumber)
+        {
+            lines.Add(element.ToString());
+            Console.WriteLine(lines[0]);
+            i = 0;
+
+            while (i <= linesnumber)
+            {
+                foreach (char elem in lines[i])
+                {
+                    int el = int.Parse(elem.ToString());
+                    numbers.Add(el);
+                }
+                var add = NewString(numbers);
+                lines.Add(add);
+                Console.WriteLine(lines[i + 1]);
+                i++;
+                numbers.Clear();
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Генерация следующей строки
+        /// </summary>
+        /// <param name="first">Лист значений предыдущей строки</param>
+        /// <returns>Возвращается готовая новая строка</returns>
+        public static string NewString(List<int> first)
+        {
+            StringBuilder line = new StringBuilder();
+
+            int count = 1;
+
+            if (first.Count == 1)
+            {
+                line.Append(1);
+                line.Append(first[0]);
+                return line.ToString();
+            }
+
+            for (int i = 0; i < first.Count - 1; i++)
+            {
+                if ((first[i] != first[i + 1] && first.Count != 1) || i == first.Count - 1)
+                {
+                    line.Append(count);
+                    line.Append(first[i]);
+                    count = 0;
+                }
+                count++;
+            }
+
+            line.Append(count);
+            line.Append(first[first.Count - 1]);
+            return line.ToString();
+        }
+    }
+}
